22cb88ea475f1a56d7790ea4d96f0a25064b64b1
Arthur <arthurmendescouto16@gmail.com>
2025-02-03 13:18:16 -0300

    update cors 2

--- src/main/java/com/devmendes/dslist/config/WebConfig.java
+++ src/main/java/com/devmendes/dslist/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.devmendes.dslist.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,18 +8,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig {
 
-    @Value("${cors.origins}")
-    private String corsOrigins;
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // Permite todas as rotas
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
-                        .allowedOrigins(corsOrigins.split(",")) // Origem permitida (Vercel + locais)
-                        .allowCredentials(false); // Sem credenciais, já que você não está usando
+                // Configuração global para permitir qualquer origem
+                registry.addMapping("/**")  // Configuração para todas as rotas
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Métodos permitidos
+                        .allowedOrigins("*")  // Permite todas as origens
+                        .allowCredentials(false);  // Não permite envio de credenciais
             }
         };
     }
